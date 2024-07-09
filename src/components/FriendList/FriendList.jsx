cec5fc473e587d0a4a9e53417ec6e971f35e9d64
13DDC <150695013+13DDC@users.noreply.github.com>
2024-07-09 15:38:07 +0300

    end hw

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,21 @@
+import s from "./FriendList.module.css";
+import FriendListItem from "./FriendListItem";
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={s.friends}>
+      {friends.map((friend) => {
+        return (
+          <li key={friend.id}>
+            <FriendListItem
+              avatar={friend.avatar}
+              name={friend.name}
+              isOnline={friend.isOnline}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default FriendList;
